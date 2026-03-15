b16a0b299bdf2427c35b0a91142deed2048cbd2c
m <m@example.com>
2026-03-15 12:35:00 +0300

    file upload backend

--- app/api/auth.py
+++ app/api/auth.py
@@ -72,41 +72,10 @@ def verify_refresh_token(token: str) -> str:
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=401, detail="Неверный токен")
 
-def get_password_hash(password):
-    return pwd_context.hash(password)
 
 def is_password_correct(password, hashed_password):
     return pwd_context.verify(password, hashed_password)
 
-
-@router.post('/register', summary='Регистрация')
-async def register_user(
-        user: UserRegister,
-        session: SessionDep
-):
-    if user.password != user.password_repeat:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Пароли не совпадают')
-
-    hashed_password = get_password_hash(password=user.password)
-
-    new_user = User(
-        username=user.username,
-        first_name=user.first_name,
-        last_name=user.last_name,
-        hashed_password=hashed_password,
-    )
-    try:
-        session.add(new_user)
-        await session.commit()
-
-    except IntegrityError:
-        await session.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Пользователем с таким логином уже существует'
-        )
-
-    return {'message': 'Пользователь создан'}
 
 
 @router.post('/login',summary='Логин')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.admin import router as admin_router
 from app.api.auth import router as auth_router
+from app.api.data import router as data_router
 from contextlib import asynccontextmanager
 from app.core.database import engine, Model
 
@@ -29,6 +30,8 @@ app.add_middleware(
 )
 
 app.include_router(auth_router)
+app.include_router(admin_router)
+app.include_router(data_router)
 @app.get('/')
 async def start():
     return 'privet'
