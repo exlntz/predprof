e63f1a74660ab3f88b1e6134b2bf66a4fb43645c
maksem <korolew.max@gmail.com>
2026-03-15 12:16:07 +0300

    1

--- web/main.js
+++ web/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import app from './app.vue'
+import App from './App.vue'
 import router from './utils/router' // Импорт твоего роутера
 
-const app = createApp(app)
+const app = createApp(App)
 app.use(router) // БЕЗ ЭТОГО БУДЕТ БЕЛЫЙ ЭКРАН
 app.mount('#app')
