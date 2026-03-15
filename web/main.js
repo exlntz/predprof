a2ce362d5c5ae291da8b7b6a1531b4b52ced9651
maksem <korolew.max@gmail.com>
2026-03-15 12:33:41 +0300

    frontend fix

--- web/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './utils/router' // Импорт твоего роутера
-
-const app = createApp(App)
-app.use(router) // БЕЗ ЭТОГО БУДЕТ БЕЛЫЙ ЭКРАН
-app.mount('#app')
